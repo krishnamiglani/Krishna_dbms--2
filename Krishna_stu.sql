29fa87e76b1034d5c94381f0e77307d535be3b04
krishnamiglani <krishnamiglani1234@gmail.com>
2026-08-15 21:56:17 +0530

    Insert sample data into student table
    Added sample student records to the student table.

--- Krishna_stu.sql
+++ Krishna_stu.sql
@@ -8,3 +8,5 @@ email VARCHAR(50) UNIQUE,
 aadhar_no VARCHAR(12) UNIQUE,
 FOREIGN KEY (dept_id) REFERENCES department9 (dept_id)
 );
+insert into student values (001, 'om' , 'omwagmare@123gmail.com' , 123456789000, 1);
+insert into student values (002, 'ram' , 'ramwanode@123gmail.com' , 000987654321, 2);
